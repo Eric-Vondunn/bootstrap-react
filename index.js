42b16cc591849474e8f3e48dc75fb746e5f32b35
Eric Vondunn <ericvondunn40@gmail.com>
2021-05-29 16:12:47 -0400

    working button

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-let x = 2;
+//upside down pyramid
+
+// let starCounter = 2 * 5 - 1;
+// for (let i = 1; i <= 5; ++i) {
+//   for (let j = 1; j < i; ++j) {
+//     process.stdout.write(" ");
+//   }
+//   for (let k = 1; k <= starCounter; ++k) {
+//     process.stdout.write("*");
+//   }
+//   starCounter = starCounter - 2;
+//   process.stdout.write("\n");
+// }
+
+// working button
+let checkUp = () => {
+  document.getElementById("click").onclick = alert("You up?");
+};
